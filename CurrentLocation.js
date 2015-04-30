4080e6a8ba5ad47fa1c87ef45b424161dbb18580
khanani92 <muddassir_92@hotmail.com>
2015-04-30 19:05:46 +0500

    1) Adding the Strucute for react.JS 2) Adding needed dependencies 3) main file App.js added and main class and functionalty added 4) CurrentLocation class added as currentLocation.js

---
+++ CurrentLocation.js
@@ -0,0 +1,22 @@
+/**
+ * Created by muddassir on 24/4/15.
+ */
+var React = require('react');
+
+var CurrentLocation = React.createClass({
+toggleFavorite(){
+    this.props.onFavoriteToggle(this.props.address)
+},
+    render(){
+        var starClassName = "glyphicon glyphicon-star-empty";
+        if(this.props.favorite){
+            starClassName = "glyphicon glyphicon-star";
+        }
+        return (
+            <div className="col-xs-12 col-md-6 col-md-offset-3 current-location">
+                <h4 id="save-location">{this.props.address}</h4>
+                <span className={starClassName} onClick={this.toggleFavorite} aria-hidden="true"></span>
+            </div>
+        )
+    }
+})
---
+++ LocationList.js
@@ -0,0 +1,12 @@
+/**
+ * Created by muddassir on 28/4/15.
+ */
+var React = require('react');
+var LocationItem = require('./LocationItem')
+
+var LocationList = React.createClass({
+    render(){
+        var self= this;
+
+    }
+})
