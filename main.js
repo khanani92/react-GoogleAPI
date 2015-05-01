1741e5fc79c25190d10351a8312cee47d3e7ffc0
khanani92 <muddassir_92@hotmail.com>
2015-05-02 01:09:34 +0500

    Other class added like CurrentLocation, LocationItem, LocationList,Map and other need file

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by muddassir on 1/5/15.
+ */
+var React = require('react');
+var App = require('./components/App');
+
+React.render(
+    <App />,
+    document.getElementById('main')
+);
